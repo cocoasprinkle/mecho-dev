e552e466ffdb6e376b941e1f5f6e84a79e5a65b7
cocoasprinkle <mochanitrate@gmail.com>
2024-03-01 11:56:24 +0000

    FINALLY THE MOVEMENT IS GOOD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,50 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables that have [SerializeField] proceeding them can be edited or manipulated in the Inspector, while keeping them private. Headers are used to organise these variables in the Inspector
     [Header("References")]
     [SerializeField] Animator anim;
     [SerializeField] CinemachineFreeLook freeLook;
 
-    [Header("Settings")]
-    [SerializeField] float speed = 5f;
+    [Header("Movement Settings")]
+    [SerializeField] float maxSpeed = 5f;
     [SerializeField] float rotSpeed = 15f;
     [SerializeField] float smoothTime = 0.2f;
-    [SerializeField] float jumpHeight = 7f;
+    [SerializeField] float coyoteDuration = 0.2f;
 
-    [Header("Ground Check Vars")]
-    [SerializeField] float groundDistance = 0.08f;
-    [SerializeField] LayerMask groundLayers;
-    
-    public bool isOnGround;
+    [Header("Jump Settings")]
+    [SerializeField] float jumpForce = 7f;
+    [SerializeField] float jumpDuration = 0.5f;
+    [SerializeField] float jumpMaxHeight = 2f;
+    [SerializeField] float gravMod = 3f;
+
+    [Header("Monitor Vars")]
+    [SerializeField] bool isOnGround;
+    [SerializeField] bool rolling;
+    [SerializeField] float jumpVel;
+    [SerializeField] bool isCoyote = false;
+
     private Rigidbody rb;
 
+    // Used for when floats need to be assigned a null value
     const float ZeroF = 0f;
 
+    // Variables relating to player movement and input
     float curSpeed;
     float velocity;
     float xInput;
     float yInput;
-
     Vector3 movement;
 
+    // Locates the transform values of the camera in the scene
     Transform mainCam;
 
+    // Declares a list of timers, along with timers in that list
+    List<Timer> timers;
+    CountdownTimer jumpTimer;
+    CountdownTimer coyoteTime;
+
+    // Awake is called when the script is activated
     void Awake()
     {
+        // Gathers component and transform references needed for variables
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         mainCam = Camera.main.transform;
         freeLook.Follow = transform;
         freeLook.LookAt = transform;
+
+        // Sets up the position and direction for the free-look camera, relative to the player
         freeLook.OnTargetObjectWarped(transform, transform.position - freeLook.transform.position - Vector3.forward);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
+
+        // Extra timer declaration
+        jumpTimer = new CountdownTimer(jumpDuration);
+        coyoteTime = new CountdownTimer(coyoteDuration);
+        timers = new List<Timer>(2) { jumpTimer, coyoteTime };
 
     }
 
@@ -55,39 +76,26 @@ public class PlayerController : MonoBehaviour
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
         movement = new Vector3(xInput, 0f, yInput);
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("HoldingRoll", true);
-        }
-        if (!Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("HoldingRoll", false);
-        }
     }
 
+    // FixedUpdate is called at a fixed rate
     void FixedUpdate()
     {
-        //HandleJump();
+        HandleJump();
         HandleMovement();
+        HandleTimers();
     }
 
+    // HandleHorizontalMovement changes the direction and velocity of the player when turning
     void HandleHorizontalMovement(Vector3 adjustedDirection)
     {
-        Vector3 velocity = adjustedDirection * speed * Time.fixedDeltaTime;
+        Vector3 velocity = adjustedDirection * maxSpeed * Time.fixedDeltaTime;
         rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
     }
 
+    // HandleMovement is largely responsible for calling movement voids when the adjustedDirection variable, declared in the void, has a magnitude greater than 0
     void HandleMovement()
     {
-        if (Input.GetKey(KeyCode.Space) && isOnGround)
-        {
-            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            anim.SetTrigger("JumpTrig");
-            isOnGround = false;
-            anim.SetBool("onGround", false);
-            Debug.Log("JUMPING");
-        }
         var adjustedDirection = Quaternion.AngleAxis(mainCam.eulerAngles.y, Vector3.up) * movement;
         if (adjustedDirection.magnitude > ZeroF)
         {
@@ -99,34 +107,241 @@ public class PlayerController : MonoBehaviour
         {
             SmoothSpeed(ZeroF);
         }
-        anim.SetFloat("Speed", rb.velocity.magnitude);
+        // Triggers rolling-related code when the roll key (Left Shift) is pressed
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rolling = true;
+        }
+        else
+        {
+            rolling = false;
+        }
+        if (adjustedDirection.magnitude > 0.1f && isOnGround)
+        {
+
+        }
     }
     
+    // HandleJump handles all jump-related functions, controlling the jump's velocity overtime, stopping the player from jumping in midair, manipulating timers and changing the Rigidbody's velocity to match the jump velocity on the y axis
+    void HandleJump()
+    {
+        bool performed = false;
 
+        if (!jumpTimer.IsRunning && isOnGround)
+        {
+            jumpVel = ZeroF;
+        }
+        if (Input.GetKey(KeyCode.Space) && isOnGround && !jumpTimer.IsRunning)
+        {
+            performed = true;
+            anim.SetTrigger("JumpTrig");
+        }
+        if (Input.GetKey(KeyCode.Space) && isCoyote && !jumpTimer.IsRunning)
+        {
+            performed = true;
+            anim.SetTrigger("JumpTrig");
+        }
+        if (performed)
+        {
+            jumpTimer.Start();
+        }
+        else if (!performed && jumpTimer.IsRunning)
+        {
+            jumpTimer.Stop();
+        }
+
+        if (jumpTimer.IsRunning)
+        {
+            float launchPoint = 0.9f;
+            if (jumpTimer.Progress > launchPoint)
+            {
+                // Calculates the decreasing jump velocity using square roots of the maximum height multiplied by 2 and returning the absolute value of the y value of gravity in the project
+                jumpVel = Mathf.Sqrt(2 * jumpMaxHeight * Mathf.Abs(Physics.gravity.y));
+            }
+            else
+            {
+                jumpVel += (1 - jumpTimer.Progress) * jumpForce * Time.fixedDeltaTime;
+            }
+        }
+        else
+        {
+            jumpVel += Physics.gravity.y * gravMod * Time.fixedDeltaTime;
+        }
+        rb.velocity = new Vector3(rb.velocity.x, jumpVel, rb.velocity.z);
+    }
+
+    // HandleTimers ensures that timers tick relative to the deltaTime of the scene, as well as any functions triggered by the end of specific timers
+    void HandleTimers()
+    {
+        foreach (var timer in timers)
+        {
+            timer.Tick(Time.deltaTime);
+        }
+        if (coyoteTime.IsRunning)
+        {
+            isCoyote = true;
+        }
+        if (coyoteTime.IsFinished)
+        {
+            isCoyote = false;
+        }
+    }
+
+    // HandleRot rotates the player in the direction of movement
     void HandleRot(Vector3 adjustedDirection)
     {
             var targetRotation = Quaternion.LookRotation(adjustedDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotSpeed * Time.deltaTime);
             transform.LookAt(transform.position + adjustedDirection);
     }
+
+    // SmoothSpeed eases the rotation of HandleRot using the SmoothDamp function in Mathf and angle functions in Quaternion
     float SmoothSpeed(float value)
     {
         var adjustedDirection = Quaternion.AngleAxis(mainCam.eulerAngles.y, Vector3.up) * movement;
-        var adjustedMovement = adjustedDirection * (speed * Time.deltaTime);
-        curSpeed = Mathf.SmoothDamp(curSpeed, adjustedMovement.magnitude, ref velocity, smoothTime);
+        var adjustedMovement = adjustedDirection * (maxSpeed * Time.deltaTime);
+        if (adjustedMovement.magnitude > 0.1f)
+        {
+            curSpeed = Mathf.SmoothDamp(curSpeed, adjustedMovement.magnitude, ref velocity, smoothTime);
+        }
+        else
+        {
+            curSpeed = 0f;
+        }
         return curSpeed;
     }
+
+    // HandleAnimator is responsible for setting the values of Animator booleans and floats to be relative to corresponding variables in the script
     void HandleAnimator()
     {
         anim.SetFloat("Speed", curSpeed);
+        anim.SetBool("onGround", isOnGround);
+        anim.SetBool("HoldingRoll", rolling);
     }
 
-    void OnCollisionEnter (Collision collision)
+    // OnCollisionStay serves as the script's "ground check" by using the Capsule Collider's bounds along with collision-related variables and Vector3 functions
+    void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        var bottom = GetComponent<CapsuleCollider>().bounds.center;
+        bottom.y -= GetComponent<CapsuleCollider>().bounds.extents.y;
+        float minDist = float.PositiveInfinity;
+        float angle = 180f;
+        // Find closest point to bottom.
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            var contact = collision.GetContact(i);
+            var tempDist = Vector3.Distance(contact.point, bottom);
+            if (tempDist < minDist)
+            {
+                minDist = tempDist;
+                // Check how close the contact normal is to our up vector.
+                angle = Vector3.Angle(transform.up, contact.normal);
+            }
+        }
+        // Check if the angle is too steep.
+        if (angle <= 80f)
         {
             isOnGround = true;
-            anim.SetBool("onGround",true);
+        }
+        else
+        {
+            isOnGround = false;
         }
     }
+
+    // OnCollsionExit's purpose is to set the "isOnGround" variable to false if the player is not colliding with any objects
+    void OnCollisionExit(Collision collision)
+    {
+        isOnGround = false;
+    }
+}
+
+// This abstract class handles all timer logic, allowing for timers to be invoked and controlled by the rest of the script (courtesy of "git-amend", a YouTuber whose tutorials have been very helpful)
+public abstract class Timer
+{
+    protected float initialTime;
+    protected float Time { get; set; }
+    public bool IsRunning { get; protected set; }
+
+    public float Progress => Time / initialTime;
+
+    public Action OnTimerStart = delegate { };
+    public Action OnTimerStop = delegate { };
+
+    protected Timer(float value)
+    {
+        initialTime = value;
+        IsRunning = false;
+    }
+
+    public void Start()
+    {
+        Time = initialTime;
+        if (!IsRunning)
+        {
+            IsRunning = true;
+            OnTimerStart.Invoke();
+        }
+    }
+
+    public void Stop()
+    {
+        if (IsRunning)
+        {
+            IsRunning = false;
+            OnTimerStop.Invoke();
+        }
+    }
+
+    public void Resume() => IsRunning = true;
+    public void Pause() => IsRunning = false;
+
+    public abstract void Tick(float deltaTime);
+}
+
+// This public class handles the logic of countdown timers specifically
+public class CountdownTimer : Timer
+{
+    public CountdownTimer(float value) : base(value) { }
+
+    public override void Tick(float deltaTime)
+    {
+        if (IsRunning && Time > 0)
+        {
+            Time -= deltaTime;
+        }
+
+        if (IsRunning && Time <= 0)
+        {
+            Stop();
+        }
+    }
+
+    public bool IsFinished => Time <= 0;
+
+    public void Reset() => Time = initialTime;
+
+    public void Reset(float newTime)
+    {
+        initialTime = newTime;
+        Reset();
+    }
+}
+
+// This public class handles the logic of stopwatch timers specifically
+public class StopwatchTimer : Timer
+{
+    public StopwatchTimer() : base(0) { }
+
+    public override void Tick(float deltaTime)
+    {
+        if (IsRunning)
+        {
+            Time += deltaTime;
+        }
+    }
+
+    public void Reset() => Time = 0;
+
+    public float GetTime() => Time;
 }
